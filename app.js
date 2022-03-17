7ca5aa168c284d9150553a4791fa0bd5e3f53a98
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-03-17 17:54:49 +0530

    added add to do function

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// All selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listener 
+todoButton.addEventListener('click', addTodo);
+
+// All functions 
+function addTodo(event) {
+    // prevent defualt 
+    event.preventDefault()
+    // creating div and its class 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create LI 
+    const newToDo = document.createElement('li');
+    newToDo.innerText = "hola";
+    newToDo.classList.add('todo-item');
+    todoDiv.appendChild(newToDo);
+
+    // Completed button (Checkmark) 
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = ' <i class="fa-solid fa-badge-check"></i> ';
+    completedButton.classList.add("completed-btn");
+    todoDiv.appendChild(completedButton);
+    console.log(completedButton);
+
+    // deleted button  
+    const deletedButton = document.createElement('button');
+    deletedButton.innerHTML = ' <i class="fa-solid fa-circle-trash"></i> ';
+    deletedButton.classList.add("delete-btn");
+    todoDiv.appendChild(deletedButton);
+    console.log(deletedButton);
+
+    // Append to list 
+    todoList.appendChild(todoDiv);
+
+
+}
